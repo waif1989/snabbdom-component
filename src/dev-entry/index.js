a7b2953a69885e52b8181ca0b25cdb911c8b687d
chensiwei <chensiwei@corp.netease.com>
2018-07-27 10:26:58 +0800

    feat: separate jsx component

--- src/dev-entry/index.js
+++ src/dev-entry/index.js
@@ -1,29 +1,15 @@
-// import Vue from 'vue';
-// import MyComponent from '../my-component';
-// new Vue({
-// 	el: '#app',
-// 	components: {
-//         MyComponent
-// 	},
-// 	data: {}
-// });
+import Snabbdom from 'snabbdom-pragma';
 import {init} from 'snabbdom';
 import {default as classDefault} from 'snabbdom/modules/class';
 import {default as propsDefault} from 'snabbdom/modules/props';
 import {default as styleDefault} from 'snabbdom/modules/style';
 import {default as eventListenersDefault} from 'snabbdom/modules/eventlisteners';
-// import buble from 'buble';
-// const buble = require('buble');
-import Snabbdom from 'snabbdom-pragma';
+import DemoComponent from '../my-snabb-component';
 const patch = init([
     classDefault,
     propsDefault,
     styleDefault,
     eventListenersDefault
 ]);
-const vnode = <div>Hello World</div>;
-patch(document.getElementById('app'), vnode);
-/*
-buble.transform(vnode, {
-    jsx: 'Snabbdom.createElement'
-});*/
+patch(document.getElementById('app'), DemoComponent);
+
---
+++ src/my-snabb-component/index.js
@@ -0,0 +1,4 @@
+import Snabbdom from 'snabbdom-pragma'; // Tell .babelrc use Snabbdom.createElement, And babel will tranform JSX into Snabbdom's 'h' function.
+const EXPORT = <div>Hello World Component</div>;
+module.exports = exports = EXPORT;
+module.default = EXPORT;
